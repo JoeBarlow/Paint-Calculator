0be8921b536063c164e54a7b0055da1b8922b039
Joe Barlow <joebarlow0800@gmail.com>
2024-10-15 15:44:08 +0100

    Vertex Foundation Added
    System in place for an extendable room size and customization.

--- Paint-Calculator/Paint-Calculator/MainWindow.xaml.cs
+++ Paint-Calculator/Paint-Calculator/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 using static Paint_Calculator.RoomData;
 
 namespace Paint_Calculator
@@ -17,9 +18,36 @@ namespace Paint_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int padding = 80;
+
+        private Canvas mainCanvas;
+        private Square baseRoom;
         public MainWindow()
         {
             InitializeComponent();
+            CreateCanvas();
+            CreateBaseRoom();
+        }
+
+        private void CreateCanvas()
+        {
+            mainCanvas = new Canvas();
+            Content = mainCanvas; //Sets the main content of the window to equal the canvas
+
+            //Canvas Settings
+            mainCanvas.Background = Brushes.Gray;
+            mainCanvas.Width = Width - padding;
+            mainCanvas.Height = Height - padding;
+        }
+
+        /// <summary>
+        /// Displays the starter room using Labels and RoomData
+        /// </summary>
+        private void CreateBaseRoom()
+        {
+            baseRoom = new Square(10, 10, 10);
+            baseRoom.Canvas = mainCanvas;
+            baseRoom.ToggleCentreSquare(true);
         }
     }
 }
--- Paint-Calculator/Paint-Calculator/RoomData.cs
+++ Paint-Calculator/Paint-Calculator/RoomData.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Paint_Calculator
 {
     public static class RoomData
     {
         private static double METRIC_PAINT_RATIO = 0.1;
+        private static double CENTRE_SQUARE_SIZE = 10; //Size in pixels of debug centre square
+        private static double VERTEX_SIZE = 10; //Size in pixels of room vertices
+        private static double BASE_ROOM_PIXEL_SIZE = 300;
         public class Square
         {
+            //Room Data
             double width, length, height;
+            //Visual Display Settings
+            private Canvas outputCanvas;
+            private Label centreSquare;
+            //Shapes Data
+            private Edge[] edges;
+            private Vertex[] vertices;
 
             public Square(double width = 0, double length = 0, double height = 0)
             {
@@ -48,6 +60,25 @@ namespace Paint_Calculator
                     return Math.Round(area, 2); 
                 }
             }
+            public Canvas Canvas
+            {
+                get { return outputCanvas; }
+                set 
+                { 
+                    outputCanvas = value;
+                    UpdateVertexPositions();
+                }
+            }
+
+            public int XPos
+            {
+                get { return (int)outputCanvas.Width / 2; }
+            }
+
+            public int YPos
+            {
+                get { return (int)outputCanvas.Height / 2; }
+            }
 
             /// <summary>
             /// Calculates the required paint in litres per metre square of wall
@@ -59,16 +90,95 @@ namespace Paint_Calculator
                 double paint = TotalWallArea * METRIC_PAINT_RATIO * numCoats;
                 return Math.Round(paint, 2);
             }
+
+            /// <summary>
+            /// Debug function to see the centre of a room
+            /// </summary>
+            /// <param name="value"></param>
+            public void ToggleCentreSquare(bool value)
+            {
+                //Instantiate Centre Square if not done already
+                if (centreSquare == null)
+                {
+                    centreSquare = new Label();
+                    centreSquare.Width = CENTRE_SQUARE_SIZE;
+                    centreSquare.Height = CENTRE_SQUARE_SIZE;
+                    centreSquare.Background = Brushes.Red;
+                    outputCanvas.Children.Add(centreSquare);
+                }
+
+                if (!value) 
+                { 
+                    centreSquare.Visibility = Visibility.Hidden; 
+                    return; 
+                }
+                else
+                {
+                    centreSquare.Visibility = Visibility.Visible;
+                }
+
+                //Calculates the centre of the room and accounts for debug square size
+                double centreX = XPos - (CENTRE_SQUARE_SIZE / 2);
+                double centreY = YPos - (CENTRE_SQUARE_SIZE / 2);
+                Canvas.SetLeft(centreSquare, centreX);
+                Canvas.SetTop(centreSquare, centreY);
+            }
+
+            private void UpdateVertexPositions()
+            {
+                vertices = new Vertex[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    vertices[i] = new Vertex(outputCanvas);
+                }
+
+                int halfSize = (int)BASE_ROOM_PIXEL_SIZE / 2;
+
+                vertices[0].SetPosition(XPos - halfSize, YPos + halfSize);
+                vertices[1].SetPosition(XPos + halfSize, YPos + halfSize);
+                vertices[2].SetPosition(XPos - halfSize, YPos - halfSize);
+                vertices[3].SetPosition(XPos + halfSize, YPos - halfSize);
+            }
         }
 
+        /// <summary>
+        /// Represents the connection between 2 vertices
+        /// <para>v1 (Start) - v2 (End)</para>
+        /// </summary>
         private class Edge
         {
-
+            Vertex v1, v2;
         }
 
+        /// <summary>
+        /// Represents point on a canvas
+        /// </summary>
         private class Vertex
-        { 
-            
+        {
+            private int x, y;
+            private Label visualComponent;
+            private Canvas outputCanvas;
+
+            public Vertex(Canvas outputCanvas)
+            {
+                x = 0; 
+                y = 0;
+
+                visualComponent = new Label();
+                visualComponent.Width = VERTEX_SIZE;
+                visualComponent.Height = VERTEX_SIZE;
+                visualComponent.Background = Brushes.Black;
+                outputCanvas.Children.Add(visualComponent);
+            }
+
+            public void SetPosition(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+
+                Canvas.SetTop(visualComponent, y);
+                Canvas.SetLeft(visualComponent, x);
+            }
         }
     }
 }

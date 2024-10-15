f4a2fe8d635dc94e5ccfacbac257431ae0ae45ab
Joe Barlow <joebarlow0800@gmail.com>
2024-10-15 14:28:07 +0100

    Volume + Paint Test Pass

--- Paint-Calculator/Paint-Calculator/RoomData.cs
+++ Paint-Calculator/Paint-Calculator/RoomData.cs
@@ -9,6 +9,7 @@ namespace Paint_Calculator
 {
     public static class RoomData
     {
+        private static double METRIC_PAINT_RATIO = 0.1;
         public class Square
         {
             double width, length, height;
@@ -20,9 +21,43 @@ namespace Paint_Calculator
                 this.height = height;
             }
 
+            /// <summary>
+            /// Returns area of the floor
+            /// </summary>
             public double Area
             {
-                get { return width * length; }
+                get { return Math.Round(width * length, 2); }
+            }
+
+            /// <summary>
+            /// Returns volume of the room
+            /// </summary>
+            public double Volume
+            {
+                get { return Math.Round(width * length * height, 2); }
+            }
+
+            /// <summary>
+            /// Returns the area of all walls combined
+            /// </summary>
+            public double TotalWallArea
+            {
+                get 
+                {
+                    double area = (width * height * 2) + (length * height * 2);
+                    return Math.Round(area, 2); 
+                }
+            }
+
+            /// <summary>
+            /// Calculates the required paint in litres per metre square of wall
+            /// </summary>
+            /// <param name="coats"></param>
+            /// <returns></returns>
+            public double GetPaintRequirement(int numCoats = 1)
+            {
+                double paint = TotalWallArea * METRIC_PAINT_RATIO * numCoats;
+                return Math.Round(paint, 2);
             }
         }
 
--- Paint-Calculator/Unit Tests/UnitTest1.cs
+++ Paint-Calculator/Unit Tests/UnitTest1.cs
@@ -14,29 +14,34 @@ namespace Unit_Tests
 
             const double expected = 22.31;
             Square newRoom = new Square(4.25, 5.25, 0);
-            double result = Math.Round(newRoom.Area, 2);
+            double result = newRoom.Area;
 
             Assert.AreEqual(expected, result);
         }
 
+        [TestMethod]
         public void TestPaintQuantity()
         {
             //Data taken from: https://www.diy.com/ideas-advice/calculators/wall-painting-calculator
             //1 litre of paint per 10m^2
-            //15x5 Wall
+            //15x5 Wall - All 4 walls
 
-            const double expected = 7.5;
-            double result = 0; //Will use RoomData function
+            const double expected = 30;
+            Square newRoom = new Square(15, 15, 5);
+            double result = newRoom.GetPaintRequirement();
 
             Assert.AreEqual(expected, result);
         }
 
+        [TestMethod]
         public void TestRoomVolume()
         {
             //Data taken from: https://www.popularmechanics.com/science/a41926325/how-to-measure-volume/
+            //Width * Length * Height - 15*10*9
 
             const double expected = 1350;
-            double result = 0; //Will use RoomData function
+            Square newRoom = new Square(15, 10, 9);
+            double result = newRoom.Volume;
 
             Assert.AreEqual(expected, result);
         }
